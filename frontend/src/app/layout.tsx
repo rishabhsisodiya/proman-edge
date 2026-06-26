843dc50d2720a57216674289a65cd28ca0c80a18
Rishabh Sisodiya <rishabh.sisodiya4@gmail.com>
2026-06-26 12:16:21 +0530

    Fixed icons not visible

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
